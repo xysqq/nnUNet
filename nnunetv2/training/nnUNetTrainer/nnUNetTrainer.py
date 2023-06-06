f8a89f27cb4eb04405521c912bf5c7ad273dec53
xys <80815268+xysqq@users.noreply.github.com>
2023-06-06 23:33:07 +0800

    Update nnUNetTrainer.py

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -3,11 +3,11 @@ import multiprocessing
 import os
 import shutil
 import sys
+import warnings
 from copy import deepcopy
 from datetime import datetime
 from time import time, sleep
 from typing import Union, Tuple, List
-import random
 
 import numpy as np
 import torch
@@ -25,7 +25,8 @@ from torch._dynamo import OptimizedModule
 from nnunetv2.configuration import ANISO_THRESHOLD, default_num_processes
 from nnunetv2.evaluation.evaluate_predictions import compute_metrics_on_folder
 from nnunetv2.inference.export_prediction import export_prediction_from_logits, resample_and_save
-from nnunetv2.inference.sliding_window_prediction import compute_gaussian, predict_sliding_window_return_logits
+from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
+from nnunetv2.inference.sliding_window_prediction import compute_gaussian
 from nnunetv2.paths import nnUNet_preprocessed, nnUNet_results
 from nnunetv2.training.data_augmentation.compute_initial_patch_size import get_patch_size
 from nnunetv2.training.data_augmentation.custom_transforms.cascade_transforms import MoveSegAsOneHotToData, \
@@ -50,7 +51,7 @@ from nnunetv2.training.loss.dice import get_tp_fp_fn_tn, MemoryEfficientSoftDice
 from nnunetv2.training.lr_scheduler.polylr import PolyLRScheduler
 from nnunetv2.utilities.collate_outputs import collate_outputs
 from nnunetv2.utilities.default_n_proc_DA import get_allowed_n_proc_DA
-from nnunetv2.utilities.file_path_utilities import check_workers_busy
+from nnunetv2.utilities.file_path_utilities import check_workers_alive_and_busy
 from nnunetv2.utilities.get_network_from_plans import get_network_from_plans
 from nnunetv2.utilities.helpers import empty_cache, dummy_context
 from nnunetv2.utilities.label_handling.label_handling import convert_labelmap_to_one_hot, determine_num_input_channels
@@ -61,7 +62,6 @@ from torch import distributed as dist
 from torch.cuda import device_count
 from torch.cuda.amp import GradScaler
 from torch.nn.parallel import DistributedDataParallel as DDP
-from pathlib import Path
 
 
 class nnUNetTrainer(object):
@@ -521,38 +521,13 @@ class nnUNetTrainer(object):
                 self.print_to_log_file("Creating new 5-fold cross-validation split...")
                 splits = []
                 all_keys_sorted = np.sort(list(dataset.keys()))
-
-                patient_id_list = sorted(set(key.split('_')[0] for key in dataset.keys()))
-                n = len(patient_id_list)
-                for start_index in [0, 0.2, 0.4, 0.6, 0.8]:
-                    if start_index == 0.8:
-                        train_patient_id_list = patient_id_list[:int(start_index * n)]
-                        test_patient_id_list = patient_id_list[int(start_index * n):]
-                    else:
-                        train_patient_id_list = patient_id_list[:int(start_index * n)] + patient_id_list[
-                                                                                         int((start_index + 0.2) * n):]
-                        test_patient_id_list = patient_id_list[int(start_index * n):int((start_index + 0.2) * n)]
-
-                    train_keys = [key for key in dataset.keys() if key.split('_')[0] in train_patient_id_list]
-                    test_keys = [key for key in dataset.keys() if key.split('_')[0] in test_patient_id_list]
-
-                    state = random.getstate()
-                    random.seed(42)
-                    random.shuffle(train_keys)
-                    random.shuffle(test_keys)
-                    random.setstate(state)
-
+                kfold = KFold(n_splits=5, shuffle=True, random_state=12345)
+                for i, (train_idx, test_idx) in enumerate(kfold.split(all_keys_sorted)):
+                    train_keys = np.array(all_keys_sorted)[train_idx]
+                    test_keys = np.array(all_keys_sorted)[test_idx]
                     splits.append({})
-                    splits[-1]['train'] = train_keys
-                    splits[-1]['val'] = test_keys
-
-                # kfold = KFold(n_splits=5, shuffle=True, random_state=12345)
-                # for i, (train_idx, test_idx) in enumerate(kfold.split(all_keys_sorted)):
-                #     train_keys = np.array(all_keys_sorted)[train_idx]
-                #     test_keys = np.array(all_keys_sorted)[test_idx]
-                #     splits.append({})
-                #     splits[-1]['train'] = list(train_keys)
-                #     splits[-1]['val'] = list(test_keys)
+                    splits[-1]['train'] = list(train_keys)
+                    splits[-1]['val'] = list(test_keys)
                 save_json(splits, splits_file)
 
             else:
@@ -1117,9 +1092,15 @@ class nnUNetTrainer(object):
         self.set_deep_supervision_enabled(False)
         self.network.eval()
 
-        num_seg_heads = self.label_manager.num_segmentation_heads
+        predictor = nnUNetPredictor(tile_step_size=0.5, use_gaussian=True, use_mirroring=True,
+                                    perform_everything_on_gpu=True, device=self.device, verbose=False,
+                                    verbose_preprocessing=False, allow_tqdm=False)
+        predictor.manual_initialization(self.network, self.plans_manager, self.configuration_manager, None,
+                                        self.dataset_json, self.__class__.__name__,
+                                        self.inference_allowed_mirroring_axes)
 
         with multiprocessing.get_context("spawn").Pool(default_num_processes) as segmentation_export_pool:
+            worker_list = [i for i in segmentation_export_pool._pool]
             validation_output_folder = join(self.output_folder, 'validation')
             maybe_mkdir_p(validation_output_folder)
 
@@ -1139,13 +1120,14 @@ class nnUNetTrainer(object):
                 _ = [maybe_mkdir_p(join(self.output_folder_base, 'predicted_next_stage', n)) for n in next_stages]
 
             results = []
+
             for k in dataset_val.keys():
-                proceed = not check_workers_busy(segmentation_export_pool, results,
-                                                 allowed_num_queued=2 * len(segmentation_export_pool._pool))
+                proceed = not check_workers_alive_and_busy(segmentation_export_pool, worker_list, results,
+                                                 allowed_num_queued=2)
                 while not proceed:
                     sleep(0.1)
-                    proceed = not check_workers_busy(segmentation_export_pool, results,
-                                                     allowed_num_queued=2 * len(segmentation_export_pool._pool))
+                    proceed = not check_workers_alive_and_busy(segmentation_export_pool, worker_list, results,
+                                                     allowed_num_queued=2)
 
                 self.print_to_log_file(f"predicting {k}")
                 data, seg, properties = dataset_val.load_case(k)
@@ -1153,29 +1135,21 @@ class nnUNetTrainer(object):
                 if self.is_cascaded:
                     data = np.vstack((data, convert_labelmap_to_one_hot(seg[-1], self.label_manager.foreground_labels,
                                                                         output_dtype=data.dtype)))
+                with warnings.catch_warnings():
+                    # ignore 'The given NumPy array is not writable' warning
+                    warnings.simplefilter("ignore")
+                    data = torch.from_numpy(data)
 
                 output_filename_truncated = join(validation_output_folder, k)
 
                 try:
-                    prediction = predict_sliding_window_return_logits(self.network, data, num_seg_heads,
-                                                                      tile_size=self.configuration_manager.patch_size,
-                                                                      mirror_axes=self.inference_allowed_mirroring_axes,
-                                                                      tile_step_size=0.5,
-                                                                      use_gaussian=True,
-                                                                      precomputed_gaussian=None,
-                                                                      perform_everything_on_gpu=True,
-                                                                      verbose=False,
-                                                                      device=self.device).cpu().numpy()
+                    prediction = predictor.predict_sliding_window_return_logits(data)
                 except RuntimeError:
-                    prediction = predict_sliding_window_return_logits(self.network, data, num_seg_heads,
-                                                                      tile_size=self.configuration_manager.patch_size,
-                                                                      mirror_axes=self.inference_allowed_mirroring_axes,
-                                                                      tile_step_size=0.5,
-                                                                      use_gaussian=True,
-                                                                      precomputed_gaussian=None,
-                                                                      perform_everything_on_gpu=False,
-                                                                      verbose=False,
-                                                                      device=self.device).cpu().numpy()
+                    predictor.perform_everything_on_gpu = False
+                    prediction = predictor.predict_sliding_window_return_logits(data)
+                    predictor.perform_everything_on_gpu = True
+
+                prediction = prediction.cpu()
 
                 # this needs to go into background processes
                 results.append(
@@ -1212,7 +1186,7 @@ class nnUNetTrainer(object):
                         output_folder = join(self.output_folder_base, 'predicted_next_stage', n)
                         output_file = join(output_folder, k + '.npz')
 
-                        # resample_and_save(prediction, target_shape, output_file, self.plans, self.configuration, properties,
+                        # resample_and_save(prediction, target_shape, output_file, self.plans_manager, self.configuration_manager, properties,
                         #                   self.dataset_json)
                         results.append(segmentation_export_pool.starmap_async(
                             resample_and_save, (
@@ -1238,8 +1212,7 @@ class nnUNetTrainer(object):
                                                 self.label_manager.foreground_labels,
                                                 self.label_manager.ignore_label, chill=True)
             self.print_to_log_file("Validation complete", also_print_to_console=True)
-            self.print_to_log_file("Mean Validation Dice: ", (metrics['foreground_mean']["Dice"]),
-                                   also_print_to_console=True)
+            self.print_to_log_file("Mean Validation Dice: ", (metrics['foreground_mean']["Dice"]), also_print_to_console=True)
 
         self.set_deep_supervision_enabled(True)
         compute_gaussian.cache_clear()
